285a30e7c40088abfb10e0989e6aab3e2af01ae6
Kudo <phuangpila101@hotmail.com>
2017-03-08 22:32:58 +0700

    m
    m

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+include('include/db.php');
+include('include/connect.php');
+?>
+							<div class="modal-header">
+						        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						        <h4 class="modal-title" id="myModalLabel">ff</h4>
+						      </div>
+						      <div class="modal-body">
+						       <?php
+						       echo $_GET['idup'];
+						       ?>
+						      </div>
+						      <div class="modal-footer">
+						        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						        <button type="button" class="btn btn-primary">Save changes</button>
+						      </div>
